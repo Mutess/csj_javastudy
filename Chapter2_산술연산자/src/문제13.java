658342bb17b9c658d262c9574639e0702c51a207
SIST <SIST@DESKTOP-VVHOPBQ>
2023-03-30 16:58:34 +0900

    2023-03-30 문제풀이

--- Chapter2_산술연산자/src/문제13.java
+++ Chapter2_산술연산자/src/문제13.java
@@ -4,7 +4,7 @@ public class 문제13 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.println("세개의 정수를 입력하세요: ");
+		System.out.print("세개의 정수를 입력하세요: ");
 		
 		int i = scan.nextInt();
 		int j = scan.nextInt();
@@ -13,7 +13,7 @@ public class 문제13 {
 		
 		int mix = i * j + k;
 		
-		System.out.println(mix);
+		System.out.println("3개의 정수를 곱하고 합한 값은 : " + mix);
 		
 
 	}
