0664a0d6f7f687df88284bfe2e8b39e0b438a14f
SIST <SIST@DESKTOP-VVHOPBQ>
2023-04-10 08:58:09 +0900

    2023-04-07

--- 2주차정리/src/파일읽기.java
+++ 2주차정리/src/파일읽기.java
@@ -2,7 +2,7 @@ import java.io.FileReader;
 
 public class 파일읽기 {
 	public static void main(String[] args) {
-		System.out.println("while사용");
+		System.out.println("페이지읽기");
 		try {
 			//FileWriter = 파일 쓰기 ,, FileReader = 파일읽기
 			FileReader fr = new FileReader("C:\\javaDev\\javaStudy\\2주차정리\\src\\제어문정리_2.java");
